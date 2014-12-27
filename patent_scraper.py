5e73670a4840c9abe68d338873a34624ef2ce5d5
Kamal <kamal.benkiran@gmail.com>
2014-12-27 19:31:07 +0000

    reference, citation and classification features

--- patent_scraper.py
+++ patent_scraper.py
@@ -4,6 +4,7 @@ import re
 import csv
 import logging
 import conf 
+import time
 
 class Patent:
 
@@ -13,7 +14,7 @@ class Patent:
 	def __init__(self,numero):
 		self.id = numero
 		self.title = ""
-		self.assignee = ""
+		self.assignee = []
 		self.inventors = []
 		self.publication_date = ""
 		self.summary = ""
@@ -25,17 +26,22 @@ class Patent:
 		self.liste_num.append(numero)
 		self.liste.append(self)
 
-patents_scraped = [] 
-for patent_id in conf.patent_ids: 
 
-	url = "http://google.com/patents/" + str(patent_id)
-	url = ''
-	url = 'http://www.google.fr/patents/US20030191577'
-	url = 'http://www.google.fr/patents/US8538675'
-	url = 'http://www.google.fr/patents/US20120079076'
-	#url = 'http://www.google.fr/patents/US20130070783'
+
+patents_scraped = [] 
+for patent_id in patent_ids[0:15]: 
+
+	url = "http://www.google.fr/patents/" + str(patent_id)
+	#.fr ou .com ??? ça change la langue des balises et des string : citations de brevets VS patent citations 
+	'''
+	url = 'http://www.google.fr/patents/US3675209'
+	url = 'http://www.google.fr/patents/US7117308'
 	url = 'http://www.google.fr/patents/US20020110134'
-	#.fr ou .com ??? ça change la langue des balises et des string : citations de brevets VS patent citations
+	url = 'http://www.google.fr/patents/US20120079076'
+	url = 'http://www.google.fr/patents/US8538675'
+	url = 'http://www.google.fr/patents/US20030191577'
+	url = 'http://www.google.fr/patents/US6979146'
+	'''
 
 	page = rq.get(url)
 	data = ""
@@ -52,17 +58,56 @@ for patent_id in conf.patent_ids:
 
 	#Partie Kamal : citations et références 
 
-	### Patents that are referenced  
+	patent.reference = get_cited_patent(soup)
+	patent.reference_by = get_citing_patent(soup)
+	patent.international_classification = get_classification(soup)
+	patents_scraped.append(patent)
+	time.sleep(3)
+
+def get_citing_patent(soup):
+
+	citing_patent = []		
+	balises = soup.find('span', text = ' Référencé par', attrs = {'class' : 'patent-section-title'})
+	if balises:
+		balises = balises.next.next
+		bal = balises.find_all('a')
+		for b in bal:
+			citing_patent.append(b.get_text().encode('ascii','ignore'))		
+
+	return citing_patent	
+
+
+def get_cited_patent(soup):
+
 	cited_patent = []
-	citing_patent = []
+	balises = soup.find('span', text = 'Citations de brevets', attrs = {'class' : 'patent-section-title'})
+	if balises:
+		balises = balises.next.next
+		bal = balises.find_all('a')
+		for b in bal:
+			cited_patent.append(b.get_text().encode('ascii','ignore'))
+
+	return cited_patent	
 
 
-	#references_html = soup.find_all('span', {'class' : 'patent-section-title'})
+def get_classification(soup):
+	
+	classification = []
+	classification_html = soup.find('td', text = ' Classification internationale', attrs = {'class' : 'patent-data-table-td '})
+	if classification_html:
+		classification_html = classification_html.next.next
+		for item in classification_html.find_all(href=True):
+			classification.append(item.get_text().encode('ascii','ignore'))
+
+	return classification	
+
+
+'''
+Ancienne version : 
+
 	references_html = soup.find_all('div',{'class' : {'patent-section-header'}})
 		
 	table_titles = []
-	#for ref in references_html:
-	#	table_titles.append(ref.get_text().encode('ascii','ignore').replace(' ',''))
 
 	for ref in references_html:
 		table_titles.append(ref.get_text().encode('ascii','ignore').replace(' ',''))
@@ -96,7 +141,14 @@ for patent_id in conf.patent_ids:
 		cited_patent_html_ = cited_patent_html.find_all('td', {'class' : 'patent-data-table-td citation-patent'})
 		for item in cited_patent_html_:
 			cited_patent.append(item.next.get_text().encode('ascii','ignore').split(' ')[0])
-
 		return cited_patent
+'''
+
+
+
+		
+
+
+
 
 	
