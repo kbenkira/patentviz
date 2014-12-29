8d74fe7352d5033c33503b9e28052ae4441eba54
Kamal <kamal.benkiran@gmail.com>
2014-12-29 19:00:42 +0000

    fixing encoding bug

--- patent_scraper.py
+++ patent_scraper.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 import requests as rq
 from bs4 import BeautifulSoup
 import re 
@@ -27,68 +29,10 @@ class Patent:
 		self.liste_num.append(numero)
 		self.liste.append(self)
 
-
-
-patents_scraped = [] 
-for patent_id in patent_ids[0:15]: 
-
-	url = "http://www.google.fr/patents/" + str(patent_id)
-	#.fr ou .com ??? ça change la langue des balises et des string : citations de brevets VS patent citations 
-	'''
-	url = 'http://www.google.fr/patents/US3675209'
-	url = 'http://www.google.fr/patents/US7117308'
-	url = 'http://www.google.fr/patents/US20020110134'
-	url = 'http://www.google.fr/patents/US20120079076'
-	url = 'http://www.google.fr/patents/US8538675'
-	url = 'http://www.google.fr/patents/US20030191577'
-	url = 'http://www.google.fr/patents/US6979146'
-	'''
-
-	page = rq.get(url)
-	data = ""
-	data += page.content + "\n"
-	soup = BeautifulSoup(data)
-
-	
-	patent = Patent(patent_id)
-
-	#Partie Othmane : détails généraux 
-	patent.title=soup.find('invention-title').get_text().encode('ascii','ignore')
-	patent.publication_date= soup.find_all('td', {'class':"single-patent-bibdata"})[3].get_text().encode('ascii','ignore')
-	patent.summary = soup.find('div', {'class':"abstract"}).get_text().encode('ascii','ignore')
-	inventors = []
-	assignee = []
-	info = soup.find_all('span', {'class':"patent-bibdata-value"})
-	for a in info:
-		if a.find('a') != None:
-			if a.find('a').get('href') != None:
-				le_href = a.find('a').get('href')
-				if "ininventor" in le_href: 
-					inventors.append(a.get_text().encode('ascii','ignore'))
-				if "inassignee" in le_href: 
-					assignee.append(a.get_text().encode('ascii','ignore'))
-
-	patent.inventors = inventors
-	patent.assignee = assignee
-
-	#Partie Kamal : citations et références 
-
-	patent.reference = get_cited_patent(soup)
-	patent.reference_by = get_citing_patent(soup)
-	patent.international_classification = get_classification(soup)
-	patents_scraped.append(patent)
-	time.sleep(3)
-
-#Dump to json
-
-print json.dumps(patents_scraped)
-out = open("brevets.json", "w")	
-out.write(json.dumps(patents_scraped))
-
 def get_citing_patent(soup):
 
 	citing_patent = []		
-	balises = soup.find('span', text = ' Référencé par', attrs = {'class' : 'patent-section-title'})
+	balises = soup.find('span', text = " Référencé par", attrs = {'class' : 'patent-section-title'})
 	if balises:
 		balises = balises.next.next
 		bal = balises.find_all('a')
@@ -121,6 +65,60 @@ def get_classification(soup):
 			classification.append(item.get_text().encode('ascii','ignore'))
 
 	return classification	
+
+
+patents_scraped = [] 
+for patent_id in patent_ids[0:15]: 
+
+	url = "http://www.google.fr/patents/" + str(patent_id)
+	'''
+	url = 'http://www.google.fr/patents/US3675209'
+	url = 'http://www.google.fr/patents/US7117308'
+	url = 'http://www.google.fr/patents/US20020110134'
+	url = 'http://www.google.fr/patents/US20120079076'
+	url = 'http://www.google.fr/patents/US8538675'
+	url = 'http://www.google.fr/patents/US20030191577'
+	url = 'http://www.google.fr/patents/US6979146'
+	'''
+
+	page = rq.get(url)
+	data = ""
+	data += page.content + "\n"
+	soup = BeautifulSoup(data)
+
+	
+	patent = Patent(patent_id)
+
+	patent.title=soup.find('invention-title').get_text().encode('ascii','ignore')
+	patent.publication_date= soup.find_all('td', {'class':"single-patent-bibdata"})[3].get_text().encode('ascii','ignore')
+	patent.summary = soup.find('div', {'class':"abstract"}).get_text().encode('ascii','ignore')
+	inventors = []
+	assignee = []
+	info = soup.find_all('span', {'class':"patent-bibdata-value"})
+	for a in info:
+		if a.find('a') != None:
+			if a.find('a').get('href') != None:
+				le_href = a.find('a').get('href')
+				if "ininventor" in le_href: 
+					inventors.append(a.get_text().encode('ascii','ignore'))
+				if "inassignee" in le_href: 
+					assignee.append(a.get_text().encode('ascii','ignore'))
+
+	patent.inventors = inventors
+	patent.assignee = assignee
+
+	patent.reference = get_cited_patent(soup)
+	patent.reference_by = get_citing_patent(soup)
+	patent.international_classification = get_classification(soup)
+	patents_scraped.append(patent)
+	time.sleep(3)
+
+#Dump to json
+
+print json.dumps(patents_scraped)
+out = open("brevets.json", "w")	
+out.write(json.dumps(patents_scraped))
+
 
 
 '''
